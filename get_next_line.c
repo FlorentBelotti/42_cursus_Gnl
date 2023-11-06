f932e492c3367fe6bd61ff2abf9ca7494daed70a
Florent Belotti <fbelotti@c2r3p5.42perpignan.fr>
2023-11-06 18:18:32 +0100

    push 42 1

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: fbelotti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 16:11:12 by fbelotti          #+#    #+#             */
-/*   Updated: 2023/11/03 18:19:46 by fbelotti         ###   ########.fr       */
+/*   Updated: 2023/11/06 17:16:36 by fbelotti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,50 @@
 char	*get_next_line(int fd)
 {
 	static t_list	*list;
-	char			*next_line;
+	char			*char_read;
 
 	list = NULL;
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, &char_read, 0) < 0)
+		return (NULL);
+}
+
+// function that calls create_list must call for the new_node to be the
+// created node.
+
+void	*create_list(t_list **list, int fd)
+{
+	t_list	new_node;
+	char	*buffer;
+	int		char_read;
+
+	char_read = 0;
+	new_node = malloc(size_of(t_list));
+	if (!new_node)
+		return (NULL);
+	char_read = read(fd, buffer, BUFFER_SIZE);
+	if (!char_read)
+	{
+		free(buffer);
+		return ;
+	}
+	buffer[char_read] = '\0';
+	new_node->content = buffer;
+	new_node->next = NULL;
+}
+
+int	search_for_newline(t_list *list)
+{
+	char	current_node;
+	int		i;
+
+	i = 0;
+	current_node = list->content;
+	while ()
+	while (current_node[i])
+	{
+		if (current_node[i] == '\n')
+			return (1);
+		i++;
+	}
+
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,23 @@
 /*   By: fbelotti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 16:11:58 by fbelotti          #+#    #+#             */
-/*   Updated: 2023/11/03 16:16:09 by fbelotti         ###   ########.fr       */
+/*   Updated: 2023/11/06 17:16:41 by fbelotti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# ifndef BUFFER_SIZE
+# define BUFFER_SIZE 10
+# endif
+
 # include <stdlib.h>
 # include <stdio.h>
+
+typdef struct s_list {
+	char			*str_buf;
+	struct s_list	*next;
+}	t_list;
 
 #endif
